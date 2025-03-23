f2df6a338f724ac739a554a954d85c86261d0371
matublaq <mati.blaquier@gmail.com>
2025-03-23 11:23:49 +0100

    None

--- BBVA_env/bin/activate.fish
+++ BBVA_env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/matublaq/Documents/GitHub/BBVA_env
+set -gx VIRTUAL_ENV /home/matublaq/Documents/GitHub/BBVA/BBVA_env
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
